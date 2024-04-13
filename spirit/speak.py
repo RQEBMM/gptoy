9ae7f2c118343ef61477747b038dc150c0041362
Ben McCloskey <benjamin.mccloskey@gmail.com>
2024-04-13 00:21:41 -0400

    updates

--- spirit/speak.py
+++ spirit/speak.py
@@ -15,12 +15,18 @@ def ask(prompt):
     completion = client.chat.completions.create(
       model="gpt-3.5-turbo",
       messages=[
-        {"role": "system", "content": "Return python code and only python code. Analyze the given code function by function then line by line. Produce python code and only python code. Explain to yourself what it does before responding. Write python code and only python code."},
+        {"role": "system", "content": "Return python code in quotes and only python code. Analyze the given code function by function then line by line. Produce python code and only python code. Explain to yourself what it does before responding. Write python code and only python code."},
         {"role": "user", "content": prompt}
       ]
     )
-    
-    return completion.choices[0].message
+    ret =  completion.choices[0].message.content
+    try:
+        lchaff, ret, rchaff = ret.split("```", maxsplit=2)
+    except ValueError:
+        ret = ret
+    if ret.startswith("python"):
+        ret = ret[6:]
+    return ret
 
 def speak(thought):
     with open(memory, 'a') as f:
@@ -28,7 +34,7 @@ def speak(thought):
         f.write(thought)
     
 if __name__ == "__main__":
-    prompt = f"Given the following, write some additional python code. Only python code. \n{think()}" # TODO: a real prompt
+    prompt = f"Given the following, write something slightly more complicated than what we've done so far. Doing more things doesn't count! \n{think()}" # TODO: a real prompt
     print(prompt)
     print("Thinking...")
     completion = ask(prompt)
